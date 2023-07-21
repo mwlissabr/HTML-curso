e5c7e8046811a2c1107bf82649134a905bdca185
mwlissabr <mwlissabr@gmail.com>
2023-07-21 15:44:21 -0300

    novo arquivo de imagens

--- praticas/exercicios/form/server.js
+++ praticas/exercicios/form/server.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true}));
 
 app.post('/usuarios', (req, resp) => {
-    console.log(req.body);
+    console.log(req.body)
+    console.log(req.query);
     resp.send('<h1>Parabéns! Usuário incluído.</h1>');
 })
 
